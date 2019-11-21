3e155252b7edcda6dd30337205069e937ebea200
azu <azuciao@gmail.com>
2019-11-21 11:42:57 +0900

    feat(typescript): add d.ts
    close https://github.com/azu/sudashi-synonyms-parser/issues/1

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import { SudachiSynonymsGroup, SudachiSynonyms } from "sudashi-synonyms-parser";
+declare const SudachiSynonymsGroupList: readonly SudachiSynonymsGroup[];
+export = SudachiSynonymsGroupList
